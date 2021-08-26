a2e7995fb309ae4b8051f5b14c10e5958666e4af
lgrupido <larry@think-black-ink.com>
2021-08-26 09:46:23 -0400

    Trying again

--- www/_instantUpdateSettings.js
+++ www/_instantUpdateSettings.js
@@ -1,6 +1,6 @@
 function _instantUpdateSettings() {
 return {
-	"baseLineGUID": "07b76cf70e0f41d89749676e8508e85f",
+	"baseLineGUID": "9522746655a24a9ba72deafe7799bc77",
 	"baseURL": "http://phonegapinstantupdate.s3.amazonaws.com/",
 	"displayMessageAfterUpdate": false,
 	"message": "",
